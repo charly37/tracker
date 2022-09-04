a0650b8d962829365dac19f93f79ac6218b8f4b8
Charles Walker <charles.walker.37@gmail.com>
2022-09-04 15:02:01 -0500

    various fix

---
+++ wrap/holdings.js
@@ -0,0 +1,56 @@
+import dbConnect from '../lib/dbConnect'
+import Holding from '../models/Holding'
+
+console.log('bb1: ');
+
+
+
+async function bb() {
+    console.log('bb0: ');
+
+    await dbConnect()
+
+
+    console.log('bb: ');
+    const holdings = await Holding.find({})
+    //console.log('holdings: ',holdings);
+    let aValueByProviders = {}
+    let aTotal = 0
+    holdings.forEach(aOneHolding => {
+        //console.log('aOneHolding: ',aOneHolding);
+        //console.log('aOneHolding: ',aOneHolding);
+
+        //Something strange....like Value computed from holding != cache.....this is to find bug
+        let aValueCacheFromObj = aOneHolding.actualValueCached
+        let aValueComputedHere = 0
+
+        aOneHolding.valueSplitProviderCached.forEach(aOneProviderHoldings => {
+            //console.log('aOneProviderHoldings: ', aOneProviderHoldings);
+            let aProvider = aOneProviderHoldings.provider
+            //console.log('aProvider: ', aProvider);
+            
+
+            if (aProvider in aValueByProviders) {
+                //console.log('Know provider');
+                aValueByProviders[aProvider]=aValueByProviders[aProvider] + aOneProviderHoldings.amount
+            }
+            else {
+                //console.log('Unknow provider');
+                aValueByProviders[aProvider]=aOneProviderHoldings.amount
+            }
+            aTotal = aTotal +aOneProviderHoldings.amount
+            aValueComputedHere = aValueComputedHere + aOneProviderHoldings.amount
+
+        });
+        if (Math.abs(aValueCacheFromObj - aValueComputedHere) > 10){
+            console.log('Strange aOneHolding: ',aOneHolding);
+        }
+
+    });
+    console.log('aValueByProviders: ',aValueByProviders);
+    console.log('aTotal: ',aTotal);
+
+    return aValueByProviders
+}
+
+export default bb
---
+++ wrap/providers.js
@@ -0,0 +1,8 @@
+console.log('aa1: ');
+
+async function aa() {
+    console.log('aa: ');
+    return "aa"
+}
+
+export default aa
